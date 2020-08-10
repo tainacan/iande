720b0df918a2b3f8be49a92943fe72df2fe158fa
Leonardo <leopiccionia@gmail.com>
2020-08-10 17:41:46 -0300

    Make site name dynamic

--- src/templates/create-user.php
+++ src/templates/create-user.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div id="iande-app" class="iande">
-    <app-create-user-page institution="Museu Afro Brasil"></app-create-user-page>
+    <app-create-user-page institution="<?= get_bloginfo('name') ?>"></app-create-user-page>
 </div>
 
 <?php get_footer(); ?>
--- src/templates/login.php
+++ src/templates/login.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div id="iande-app" class="iande">
-    <app-login-page institution="Museu Afro Brasil"></app-login-page>
+    <app-login-page institution="<?= get_bloginfo('name') ?>"></app-login-page>
 </div>
 
 <?php get_footer(); ?>
