c0bee5e61457b851600bd751e007239022396427
Leonardo <leopiccionia@gmail.com>
2020-08-15 15:44:32 -0300

    Add icon

--- src/assets/js/utils/icons.js
+++ src/assets/js/utils/icons.js
@@ -1,4 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAngleLeft, faAngleRight, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faAngleLeft, faAngleRight, faCheck, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faAngleLeft, faAngleRight, faCheck)
+library.add(faAngleLeft, faAngleRight, faCheck, faPlusCircle)
