cf321baa952e84b9a92d2d22eb8559e22c6cbec8
Leonardo <leopiccionia@gmail.com>
2020-08-17 18:23:32 -0300

    Remove verification

--- src/includes/cmb2.php
+++ src/includes/cmb2.php
@@ -1,7 +1,5 @@
 <?php
 
-if ( in_array('cmb2/init.php', get_option('active_plugins')) ) {
-    require 'cmb2/helpers.php';
-    require 'cmb2/settings.php';
-    require 'cmb2/user.php';
-}
+require 'cmb2/helpers.php';
+require 'cmb2/settings.php';
+require 'cmb2/user.php';
