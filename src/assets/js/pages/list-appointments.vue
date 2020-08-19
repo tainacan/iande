4779c9eb4635e4cfaddf61c5038a12d6b8ab7b03
Leonardo <leopiccionia@gmail.com>
2020-08-19 18:10:28 -0300

    Appointments summary

--- src/assets/js/pages/list-appointments.vue
+++ src/assets/js/pages/list-appointments.vue
@@ -2,7 +2,7 @@
     <article>
         <div class="iande-container iande-stack stack-lg">
             <h1>Seus agendamentos</h1>
-            <AppointmentDetails v-for="appointment in appointments" :key="appointment.ID" :appointment="appointment"/>
+            <AppointmentDetails v-for="(appointment, n) in appointments" :key="appointment.ID" :appointment="appointment" :n="n + 1"/>
         </div>
     </article>
 </template>
