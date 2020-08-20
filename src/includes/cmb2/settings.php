2dcea936544115303a4d6a650bf297c44201d869
Everaldo Matias <matiaseveraldo@gmail.com>
2020-08-20 17:20:32 -0300

    Adds settings schedules

--- src/includes/cmb2/settings.php
+++ src/includes/cmb2/settings.php
@@ -10,7 +10,7 @@ function iande_register_theme_options_metabox() {
     $args = [
         'id'           => 'iande_main_options_page',
         'title'        => __('Iandé', 'iande'),
-        'object_types' => array('options-page'),
+        'object_types' => ['options-page'],
         'option_key'   => 'iande',
         'tab_group'    => 'iande_institution',
         'tab_title'    => __('Instituição', 'iande'),
@@ -36,9 +36,9 @@ function iande_register_theme_options_metabox() {
         'id'         => 'institution_profile',
         'type'       => 'text',
         'repeatable' => true,
-        'text'       => array(
+        'text'       => [
             'add_row_text' => __('Adicionar nova opção', 'iande'),
-        ),
+        ]
     ]);
 
     // Escolaridade
@@ -53,9 +53,9 @@ function iande_register_theme_options_metabox() {
         'id'         => 'institution_schooling',
         'type'       => 'text',
         'repeatable' => true,
-        'text'       => array(
+        'text'       => [
             'add_row_text' => __('Adicionar nova opção', 'iande'),
-        ),
+        ]
     ]);
 
     // Relação do Responsável
@@ -70,9 +70,9 @@ function iande_register_theme_options_metabox() {
         'id'         => 'institution_responsible_role',
         'type'       => 'text',
         'repeatable' => true,
-        'text'       => array(
+        'text'       => [
             'add_row_text' => __('Adicionar nova opção', 'iande'),
-        ),
+        ]
     ]);
 
     // Deficiências
@@ -87,9 +87,9 @@ function iande_register_theme_options_metabox() {
         'id'         => 'institution_deficiency',
         'type'       => 'text',
         'repeatable' => true,
-        'text'       => array(
+        'text'       => [
             'add_row_text' => __('Adicionar nova opção', 'iande')
-        )
+        ]
     ]);
 
     // Idiomas
@@ -104,9 +104,9 @@ function iande_register_theme_options_metabox() {
         'id'         => 'institution_language',
         'type'       => 'text',
         'repeatable' => true,
-        'text'       => array(
+        'text'       => [
             'add_row_text' => __('Adicionar nova opção', 'iande')
-        )
+        ]
     ]);
 
     // Faixa Etária
@@ -121,9 +121,345 @@ function iande_register_theme_options_metabox() {
         'id'         => 'institution_age_range',
         'type'       => 'text',
         'repeatable' => true,
-        'text'       => array(
+        'text'       => [
             'add_row_text' => __('Adicionar nova opção', 'iande')
-        )
+        ]
+    ]);
+
+    /**
+     * Hour settings tab
+     */
+    $args = [
+        'id'           => 'iande_schedules_options_page',
+        'title'        =>  __('Configuração de Horários', 'iande'),
+        'object_types' => ['options-page'],
+        'option_key'   => 'iande_schedules',
+        'parent_slug'  => 'iande',
+        'tab_group'    => 'iande_institution',
+        'tab_title'    => __('Horários', 'iande'),
+        'save_button'  => esc_html__('Salvar opções', 'iande')
+    ];
+
+    // 'tab_group' property is supported in > 2.4.0.
+    if (version_compare(CMB2_VERSION, '2.4.0')) {
+        $args['display_cb'] = 'iande_options_display_with_tabs';
+    }
+
+    $iande_schedules_options = new_cmb2_box($args);
+
+    /**
+     * Monday
+     */
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'name' => __('Terça-feira', 'iande'),
+        'id'   => 'monday_title',
+        'type' => 'title'
+    ]);
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'id'          => 'monday',
+        'type'        => 'group',
+        'options'     => [
+            'group_title'   => __('Horário {#}', 'iande'),
+            'add_button'    => __('Adicionar novo horário', 'iande'),
+            'remove_button' => __('Remover horário', 'iande'),
+            'closed'        => true
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('De', 'iande'),
+        'id'   => 'from',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+        	'data-timepicker' => json_encode( [
+        		'timeOnlyTitle' => __( 'Escolha o horário', 'iande' ),
+        		'timeFormat'    => 'HH:mm',
+        		'stepMinute'    => 10,
+             ] ),
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('Até', 'iande'),
+        'id'   => 'to',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+
+    /**
+     * Tuesday
+     */
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'name' => __('Terça-feira', 'iande'),
+        'id'   => 'tuesday_title',
+        'type' => 'title'
+    ]);
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'id'          => 'tuesday',
+        'type'        => 'group',
+        'options'     => [
+            'group_title'   => __('Horário {#}', 'iande'),
+            'add_button'    => __('Adicionar novo horário', 'iande'),
+            'remove_button' => __('Remover horário', 'iande'),
+            'closed'        => true
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('De', 'iande'),
+        'id'   => 'from',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('Até', 'iande'),
+        'id'   => 'to',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+
+    /**
+     * Wednesday
+     */
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'name' => __('Quarta-feira', 'iande'),
+        'id'   => 'wednesday_title',
+        'type' => 'title'
+    ]);
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'id'          => 'wednesday',
+        'type'        => 'group',
+        'options'     => [
+            'group_title'   => __('Horário {#}', 'iande'),
+            'add_button'    => __('Adicionar novo horário', 'iande'),
+            'remove_button' => __('Remover horário', 'iande'),
+            'closed'        => true
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('De', 'iande'),
+        'id'   => 'from',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('Até', 'iande'),
+        'id'   => 'to',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+
+    /**
+     * Thursday
+     */
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'name' => __('Quinta-feira', 'iande'),
+        'id'   => 'thursday_title',
+        'type' => 'title'
+    ]);
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'id'          => 'thursday',
+        'type'        => 'group',
+        'options'     => [
+            'group_title'   => __('Horário {#}', 'iande'),
+            'add_button'    => __('Adicionar novo horário', 'iande'),
+            'remove_button' => __('Remover horário', 'iande'),
+            'closed'        => true
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('De', 'iande'),
+        'id'   => 'from',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('Até', 'iande'),
+        'id'   => 'to',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+
+    /**
+     * Friday
+     */
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'name' => __('Sexta-feira', 'iande'),
+        'id'   => 'friday_title',
+        'type' => 'title'
+    ]);
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'id'          => 'friday',
+        'type'        => 'group',
+        'options'     => [
+            'group_title'   => __('Horário {#}', 'iande'),
+            'add_button'    => __('Adicionar novo horário', 'iande'),
+            'remove_button' => __('Remover horário', 'iande'),
+            'closed'        => true
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('De', 'iande'),
+        'id'   => 'from',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('Até', 'iande'),
+        'id'   => 'to',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+
+    /**
+     * Saturday
+     */
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'name' => __('Sábado', 'iande'),
+        'id'   => 'saturday_title',
+        'type' => 'title'
+    ]);
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'id'          => 'saturday',
+        'type'        => 'group',
+        'options'     => [
+            'group_title'   => __('Horário {#}', 'iande'),
+            'add_button'    => __('Adicionar novo horário', 'iande'),
+            'remove_button' => __('Remover horário', 'iande'),
+            'closed'        => true
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('De', 'iande'),
+        'id'   => 'from',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('Até', 'iande'),
+        'id'   => 'to',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+
+    /**
+     * Sunday
+     */
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'name' => __('Domingo', 'iande'),
+        'id'   => 'sunday_title',
+        'type' => 'title'
+    ]);
+    $iande_schedules_fields = $iande_schedules_options->add_field([
+        'id'          => 'sunday',
+        'type'        => 'group',
+        'options'     => [
+            'group_title'   => __('Horário {#}', 'iande'),
+            'add_button'    => __('Adicionar novo horário', 'iande'),
+            'remove_button' => __('Remover horário', 'iande'),
+            'closed'        => true
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('De', 'iande'),
+        'id'   => 'from',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
+    ]);
+    $iande_schedules_options->add_group_field($iande_schedules_fields, [
+        'name' => __('Até', 'iande'),
+        'id'   => 'to',
+        'type' => 'text_time',
+        'time_format' => 'H:i',
+        'attributes'  => [
+            'data-timepicker' => json_encode([
+                'timeOnlyTitle' => __('Escolha o horário', 'iande'),
+                'timeFormat'    => 'HH:mm',
+                'stepMinute'    => 10,
+            ]),
+        ],
     ]);
 
 }
@@ -143,6 +479,7 @@ function iande_institution_settings() {
     $deficiency       = iande_get_option('institution_deficiency',[]);
     $language         = iande_get_option('institution_language', []);
     $age_range        = iande_get_option('institution_age_range', []);
+    $schedules        = get_option('iande_schedules', []);
 
     wp_localize_script(
         'iande',
@@ -155,7 +492,8 @@ function iande_institution_settings() {
             'responsibleRoles' => $responsible_role,
             'deficiencies'     => $deficiency,
             'languages'        => $language,
-            'ageRanges'        => $age_range
+            'ageRanges'        => $age_range,
+            'schedules'        => $schedules
 
         ]
     );
