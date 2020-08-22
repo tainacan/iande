2ac2bada278987f67ac9e79e32f75395eb765113
Leonardo <leopiccionia@gmail.com>
2020-08-22 08:28:40 -0300

    Remove Vuex persistence plugin

--- src/assets/js/store/index.js
+++ src/assets/js/store/index.js
@@ -1,16 +1,9 @@
 import Vuex from 'vuex'
 import pathify from 'vuex-pathify'
-import createPersistedState from 'vuex-persistedstate'
 
 import appointments from './appointments'
 import institutions from './institutions'
 import user from './user'
-
-const persistence = createPersistedState({
-    paths: [
-        'user.user',
-    ]
-})
 
 export default function createStore() {
     return new Vuex.Store({
@@ -20,7 +13,6 @@ export default function createStore() {
             user,
         },
         plugins: [
-            persistence,
             pathify.plugin,
         ]
     })
