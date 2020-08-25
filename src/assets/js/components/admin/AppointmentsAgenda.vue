4f5f74797b0a58ad30263615d94f06e44f415798
Leonardo <leopiccionia@gmail.com>
2020-08-25 17:36:07 -0300

    On month view, show available hours and appointments count

--- src/assets/js/components/admin/AppointmentsAgenda.vue
+++ src/assets/js/components/admin/AppointmentsAgenda.vue
@@ -1,15 +1,30 @@
 <template>
     <div class="iande-appointments-agenda">
-        <Calendar activeView="month" :disableViews="['years', 'year']" locale="pt-br"/>
+        <Calendar activeView="month" :disableViews="['years', 'year']" :events="events" locale="pt-br">
+            <template #cell-content="{ cell, view }">
+                <template v-if="view.id === 'month'">
+                    <div>{{ cell.content }}</div>
+                    <div v-if="!cell.outOfScope && cellHours(cell)">
+                        {{ cellHours(cell) }}
+                    </div>
+                    <div v-if="!cell.outOfScope && cellAppointments(cell)">
+                        {{ cellAppointments(cell).length }} agendamento{{ cellAppointments(cell).length > 1 ? 's' : '' }}
+                    </div>
+                </template>
+            </template>
+        </Calendar>
     </div>
 </template>
 
 <script>
+    import { DateTime } from 'luxon'
     import Calendar from 'vue-cal'
-
     import 'vue-cal/dist/i18n/pt-br';
 
     import { api } from '../../utils'
+
+    const weekDays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday',
+        'friday', 'saturday']
 
     export default {
         name: 'AppointmentsAgenda',
@@ -23,16 +38,36 @@
         },
         computed: {
             appointmentsByDate () {
-                const dates = {}
+                const dates = new Map()
                 for (const appointment of this.appointments) {
-                    const date = dates[appointment.date]
+                    const date = dates.get(appointment.date)
                     if (date) {
                         date.push(appointment)
                     } else {
-                        dates[appointment.date] = [appointment]
+                        dates.set(appointment.date, [appointment])
                     }
                 }
                 return dates
+            },
+            events () {
+                const delta = { minutes: Number(window.IandeSettings.duration) }
+                const endsCache = new Map()
+                return this.appointments.map(appointment => {
+                    const start = appointment.hour
+                    let end
+                    if (endsCache.has(start)) {
+                        end = endsCache.get(start)
+                    } else {
+                        end = DateTime.fromFormat(start, 'HH:mm').plus(delta).toFormat('HH:mm')
+                        endsCache.set(start, end)
+                    }
+                    return {
+                        start: `${appointment.date} ${start}`,
+                        end: `${appointment.date} ${end}`,
+                        title: appointment.name || 'Agendamento',
+                        raw: appointment,
+                    }
+                })
             }
         },
         async created () {
@@ -41,6 +76,22 @@
                 this.appointments = appointments
             } catch (err) {
                 console.error(err)
+            }
+        },
+        methods: {
+            cellAppointments (cell) {
+                return this.appointmentsByDate.get(cell.formattedDate)
+            },
+            cellHours (cell) {
+                const weekDay = window.IandeSettings.schedules[weekDays[cell.startDate.getDay()]]
+                if (!weekDay) {
+                    return ''
+                } else {
+                    return weekDay
+                        .filter(interval => interval.from && interval.to)
+                        .map(interval => `${interval.from}-${interval.to}`)
+                        .join('; ')
+                }
             }
         }
     }
