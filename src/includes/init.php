8f507485f1a497d6df39a42f2265fb37f17fdfb2
Everaldo Matias <matiaseveraldo@gmail.com>
2020-08-25 08:43:27 -0300

    Remove styles and scripts default

--- src/includes/init.php
+++ src/includes/init.php
@@ -34,3 +34,45 @@ function iande_activation_plugin() {
 
 }
 add_action('admin_init', 'Iande\\iande_activation_plugin');
+
+/**
+ * Removes os styles padrões do tema
+ * 
+ * @link https://developer.wordpress.org/reference/functions/wp_dequeue_style/
+ */
+function iande_remove_default_stylesheet()
+{
+
+    global $wp_styles;
+
+    foreach ( $wp_styles->queue as $style ) :
+
+        if ($style != 'iande') {
+            \wp_dequeue_style($style);
+        }
+
+    endforeach;
+
+}
+add_action('wp_enqueue_scripts', 'Iande\\iande_remove_default_stylesheet', 999);
+
+/**
+ * Removes os scripts padrões do tema
+ * 
+ * @link https://developer.wordpress.org/reference/functions/wp_dequeue_script/
+ */
+function iande_remove_default_scripts()
+{
+
+    global $wp_scripts;
+
+    foreach ($wp_scripts->queue as $script) :
+
+        if ($script != 'iande') {
+            \wp_dequeue_script($script);
+        }
+
+    endforeach;
+
+}
+add_action('wp_enqueue_scripts', 'Iande\\iande_remove_default_scripts', 999999);
