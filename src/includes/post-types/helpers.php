b37ac702b45b49feac8fa538558cc246769a2c9a
Leonardo <leopiccionia@gmail.com>
2020-08-28 18:08:36 -0300

    Pretty-display cities and states in admin

--- src/includes/post-types/helpers.php
+++ src/includes/post-types/helpers.php
@@ -20,6 +20,46 @@ function array_post_exists ($post_id, array $array) {
 }
 
 /**
+ * Retorna as opções de municípios, de maneira compatível com o front-end
+ *
+ * @param string $state UF do estado, para filtragem
+ * @return array
+ */
+function get_city_options ($state = '') {
+    $json = \json_decode(\file_get_contents(IANDE_BASEURL . 'assets/json/municipios.json'), true);
+    $options = [];
+    if (empty($state)) {
+        $state = 'AC'; // First state, alphabetically
+    }
+
+    foreach ($json as $key => $value) {
+        if (\strpos($key, $state)  === 0) {
+            $options[$key] = $value;
+        }
+    }
+
+    \asort($options, SORT_LOCALE_STRING);
+    return $options;
+}
+
+/**
+ * Retorna as opções de estados, de maneira compatível com o front-end
+ *
+ * @return array
+ */
+function get_state_options () {
+    $json = \json_decode(\file_get_contents(IANDE_BASEURL . 'assets/json/estados.json'), true);
+    $options = [];
+
+    foreach ($json as $key => $value) {
+        $options[$key] = $key;
+    }
+
+    \ksort($options);
+    return $options;
+}
+
+/**
  * Mapeia uma array de strings para opções CMB2
  *
  * @param array $array Lista de opções
