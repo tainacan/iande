cc7133dc6601b6fad5c449fe4c9eac5f16e5006b
Leonardo <leopiccionia@gmail.com>
2020-08-28 11:49:10 -0300

    Complete navbar

--- src/assets/js/utils/icons.js
+++
@@ -1,6 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAddressCard, faCalendar, faClock } from '@fortawesome/free-regular-svg-icons'
-import { faAngleLeft, faAngleRight, faCheck, faMapMarkerAlt, faMinusCircle, faPencilAlt, faPlusCircle, faQuestionCircle, faSpinner, faTimes, faUniversity, faUser, faUsers } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faAddressCard, faCalendar, faClock)
-library.add(faAngleLeft, faAngleRight, faCheck, faMapMarkerAlt, faMinusCircle, faPencilAlt, faPlusCircle, faQuestionCircle, faSpinner, faTimes, faUniversity, faUser, faUsers)
