f85268644d994db17fce6937475706b297cbbe99
Leonardo <leopiccionia@gmail.com>
2020-09-01 20:47:28 -0300

    Add more fields to CSV

--- src/includes/csv.php
+++ src/includes/csv.php
@@ -6,7 +6,8 @@ function iande_export_button($which) {
 
     if ('appointment' === $typenow && 'top' === $which && \current_user_can( 'manage_options' )) {
         ?>
-        <input type="submit" name="export_all_posts" id="export_all_posts" class="button button-primary" value="Exportar Agendamentos" />
+        <input type="submit" name="export_all_appointments" id="export_all_appointments" class="button button-primary" value="Exportar Agendamentos" />
+        <!-- <input type="submit" name="export_all_groups" id="export_all_groups" class="button button-primary" value="Exportar Grupos" /> -->
         <?php
 	}
 
@@ -20,15 +21,14 @@ function iande_export_appointment() {
         return;
     }
 
-    if (isset($_GET['export_all_posts']) && \is_admin()) {
+    if (isset($_GET['export_all_appointments']) && \is_admin()) {
 
-        
         $args = array(
             'post_type'      => 'appointment',
             'post_status'    => 'publish',
             'posts_per_page' => -1,
         );
-                
+
         $appointments = \get_posts($args);
         if ($appointments) {
 
@@ -37,15 +37,15 @@ function iande_export_appointment() {
             // Cria o nome do CSV
             $current_datetime = \sanitize_title(date('Ymd-His', current_time('timestamp', 0)));
             $blogname = \sanitize_title(get_bloginfo('name'));
-            $filename = $blogname . '-export-appointment-' . $current_datetime . '.csv';
+            $filename = $blogname . '-export-appointments-' . $current_datetime . '.csv';
 
             // Cabeçalhos do CSV
             $header_row = array(
                 'ID',
                 'Título',
+                'Nome da visita',
                 'Objetivo da visita',
                 'Objetivo da visita (Outro)',
-                'Nome da visita',
                 'Data',
                 'Hora',
                 'Nome do responsável',
@@ -55,7 +55,11 @@ function iande_export_appointment() {
                 'Relação com a instituição',
                 'Relação com a instituição (Outro)',
                 'Natureza do grupo',
-                'Instituição'
+                'Instituição',
+                'Já visitou o museu?',
+                'Preparou a visita?',
+                'Como preparou a visita',
+                'Comentários adicionais',
             );
 
             $data_rows = array();
@@ -68,9 +72,9 @@ function iande_export_appointment() {
                 $row = array(
                     $id,
                     $appointment->post_title,
+                    \get_post_meta($id, 'name', true),
                     \get_post_meta($id, 'purpose', true),
                     \get_post_meta($id, 'purpose_other', true),
-                    \get_post_meta($id, 'name', true),
                     \get_post_meta($id, 'date', true),
                     \get_post_meta($id, 'hour', true),
                     \get_post_meta($id, 'responsible_first_name', true),
@@ -79,7 +83,12 @@ function iande_export_appointment() {
                     \get_post_meta($id, 'responsible_phone', true),
                     \get_post_meta($id, 'responsible_role', true),
                     \get_post_meta($id, 'responsible_role_other', true),
-                    \get_the_title(\get_post_meta($id, 'institution_id', true))
+                    \get_post_meta($id, 'group_nature', true),
+                    \get_the_title(\get_post_meta($id, 'institution_id', true)),
+                    \get_post_meta($id, 'has_visited_previously', true),
+                    \get_post_meta($id, 'has_prepared_visit', true),
+                    \get_post_meta($id, 'how_prepared_visit', true),
+                    \get_post_meta($id, 'additional_comment', true),
                 );
                 $data_rows[] = $row;
 
@@ -87,7 +96,7 @@ function iande_export_appointment() {
 
             $fh = @fopen('php://output', 'w');
             fprintf($fh, chr(0xEF) . chr(0xBB) . chr(0xBF));
-            
+
             header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
             header('Content-Description: File Transfer');
             header('Content-type: text/csv');
