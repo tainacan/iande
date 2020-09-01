0915b11be7cef649971fa5d2f0633b0230f3778e
Everaldo Matias <matiaseveraldo@gmail.com>
2020-09-01 14:03:57 -0300

    Adds index on parse exceptions

--- src/controllers/exhibition.php
+++ src/controllers/exhibition.php
@@ -142,12 +142,12 @@ class Exhibition extends Controller
                 foreach (unserialize($metadata[$key][0]) as $post_id) {
                     
                     $post         = get_post($post_id);
-                    $schedules    = get_post_meta($post->ID, 'exception', true);
+                    $schedules    = get_post_meta($post->ID, 'exception', false);
                     
                     $exceptions[] = [
-                        $post->ID,
-                        $post->post_title,
-                        $schedules
+                        'ID'        => $post->ID,
+                        'title'     => $post->post_title,
+                        'exceptions' => $schedules
                     ];
 
                 }
