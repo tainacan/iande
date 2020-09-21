a3598ceb35ea87cf8c591a1e134c002fe50af124
Everaldo Matias <matiaseveraldo@gmail.com>
2020-09-21 18:19:09 -0300

    Temporarily hides the post type group

--- src/includes/post-types.php
+++ src/includes/post-types.php
@@ -7,4 +7,4 @@ require 'post-types/appointment.php';
 require 'post-types/institution.php';
 require 'post-types/exhibition.php';
 require 'post-types/exception.php';
-require 'post-types/group.php';
+//require 'post-types/group.php';
