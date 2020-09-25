cfeb8e05fb965714aecbc73b93565de85b258318
Leonardo <leopiccionia@gmail.com>
2020-09-25 16:04:57 -0300

    Refactor reused modal to separate component

---
+++ src/assets/js/components/AppointmentSuccessModal.vue
@@ -0,0 +1,34 @@
+<template>
+    <Modal ref="modal" @close="close">
+        <div class="iande-stack">
+            <h1>Agendamento enviado com sucesso!</h1>
+            <p>Os dados do seu agendamento foram enviados para o museu. Assim que a sua visita for confirmada, você receberá um email com todos os detalhes.</p>
+            <button class="iande-button solid" @click="close">
+                Voltar aos agendamentos
+            </button>
+        </div>
+    </Modal>
+</template>
+
+<script>
+    import Modal from './Modal.vue'
+
+    export default {
+        name: 'AppointmentSuccessModal',
+        components: {
+            Modal,
+        },
+        methods: {
+            close () {
+                if (this.$refs.modal.isOpen) {
+                    this.$refs.modal.close()
+                }
+                window.location.assign(`${window.IandeSettings.iandeUrl}/appointment/list`)
+                window.location.reload()
+            },
+            open () {
+                this.$refs.modal.open()
+            },
+        }
+    }
+</script>
