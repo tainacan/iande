0a0ad4f36b9da0982e24ccebc6ecd1c3150dbf58
Leonardo <leopiccionia@gmail.com>
2020-10-20 12:02:02 -0300

    Upgrade to Laravel Mix v6 (WIP)

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,20 +8,11 @@ mix.js(assetsDir + 'js/admin.js', distDir)
     .js(assetsDir + 'js/app.js', distDir)
     .sass(assetsDir + 'scss/admin.scss', distDir)
     .sass(assetsDir + 'scss/app.scss', distDir)
-    .options({
-        terser: {
-            extractComments: false,
-            terserOptions: {
-                output: {
-                    comments: false,
-                },
-            },
+    .vue({
+        compilerOptions: {
+            whitespace: 'condense',
         },
-        vue: {
-            compilerOptions: {
-                whitespace: 'condense',
-            },
-        },
+        version: 2,
     })
     .webpackConfig({
         output: {
