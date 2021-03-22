44c665fa23eaf591bd68fb2d02d745cf59bde433
Leonardo <leopiccionia@gmail.com>
2021-03-22 12:32:29 -0300

    Update polyfills

--- src/includes/polyfills.php
+++ src/includes/polyfills.php
@@ -1,7 +1,12 @@
 <?php
 
-if (!function_exists('is_plugin_active')) {
-    function is_plugin_active($plugin) {
-        return in_array($plugin, (array) get_option('active_plugins', []));
+function iande_polyfills () {
+    if (!is_admin()) {
+        if (!function_exists('is_plugin_active')) {
+            function is_plugin_active($plugin) {
+                return in_array($plugin, (array) get_option('active_plugins', []));
+            }
+        }
     }
 }
+add_action('init', 'iande_polyfills');
