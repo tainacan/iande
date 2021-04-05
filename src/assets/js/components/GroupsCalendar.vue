05ad9a51388b192e27dce02a62271e771f46dfff
Leonardo <leopiccionia@gmail.com>
2021-04-05 15:53:42 -0300

    Format events in daily view

--- src/assets/js/components/GroupsCalendar.vue
+++ src/assets/js/components/GroupsCalendar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="iande-educator-agenda">
-        <Calendar activeView="month" :disableViews="['years', 'year']" :events="events" locale="pt-br" startWeekOnSunday>
+        <Calendar activeView="month" :disableViews="['years', 'year']" :events="events" locale="pt-br" startWeekOnSunday :timeFrom="timeLimits.start" :timeStep="15" :timeTo="timeLimits.end">
             <template #cell-content="{ cell, view }">
                 <template v-if="view.id === 'month'">
                     <div class="iande-admin-agenda__month">
@@ -14,6 +14,13 @@
                         </div>
                     </div>
                 </template>
+            </template>
+            <template #event="{ event }">
+                <div class="iande-educator-agenda__event unassigned">
+                    <p><b>{{ event.group.name }}</b></p>
+                    <p>{{ event.group.hour }} - {{ formatHour(event.end) }}</p>
+                    <a href="javascript:void(0)">ver mais</a>
+                </div>
             </template>
         </Calendar>
     </div>
@@ -64,13 +71,44 @@
             },
             exhibitionsList () {
                 return new Map(this.exhibitions.map(exhibition => [Number(exhibition.ID), exhibition]))
-            }
+            },
+            timeLimits () {
+                if (this.events.length === 0) {
+                    return { start: 0, end: 24 * 60 }
+                }
+
+                let start = '24:00'
+                let end = '00:00'
+
+                for (const group of this.groups) {
+                    const exhibition = this.exhibitionsList.get(Number(group.exhibition_id)) ?? 60
+                    const groupStart = group.hour
+                    const delta = { minutes: Number(exhibition.duration) }
+                    const groupEnd = DateTime.fromFormat(groupStart, 'HH:mm').plus(delta).toFormat('HH:mm')
+                    if (groupStart < start) {
+                        start = groupStart
+                    }
+                    if (groupEnd > end) {
+                        end = groupEnd
+                    }
+                }
+
+                const [startHour, startMinute] = start.split(':')
+                const [endHour, endMinute] = end.split(':')
+                return {
+                    start: (Number(startHour) * 60) + Number(startMinute),
+                    end: (Number(endHour) * 60) + Number(endMinute),
+                }
+            },
         },
         methods: {
             cellGroups (cell) {
                 const groups = this.groupsByDate.get(cell.formattedDate) || []
                 return groups
             },
+            formatHour (date) {
+                return DateTime.fromJSDate(date).toFormat('HH:mm')
+            }
         }
     }
 </script>
