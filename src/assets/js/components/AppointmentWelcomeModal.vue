be63039154d89f842ded00d6ba785268cf36a0bb
Leonardo <leopiccionia@gmail.com>
2021-05-06 15:17:04 -0300

    Support full-width containers in modals

--- src/assets/js/components/AppointmentWelcomeModal.vue
+++ src/assets/js/components/AppointmentWelcomeModal.vue
@@ -1,11 +1,11 @@
 <template>
-    <Modal large :label="__('Instruções', 'iande')" ref="modal" @close="close">
-        <div class="iande-container narrow iande-stack stack-lg">
+    <Modal narrow :label="__('Instruções', 'iande')" ref="modal" @close="close">
+        <div class="iande-stack stack-lg">
             <h1>{{ __('Agendamento de visita presencial', 'iande') }}</h1>
             <p>{{ __('Agendar uma visita presencial é bem simples e possui essas 3 etapas que apresentamos abaixo:', 'iande') }}</p>
         </div>
         <StepsIndicator/>
-        <div class="iande-container narrow iande-stack stack-lg">
+        <div class="iande-stack stack-lg">
             <p v-html="__('Na <b>etapa 1</b> solicitamos informações básicas como data, horário e contatos da pessoa ou instituição responsável pela visita. Assim realizamos a reserva da visitação.', 'iande')"/>
             <p v-html="__('Na <b>etapa 2</b> pedimos mais detalhes a respeito do grupo de visitantes, como se há necessidade de algum preparo específico como interprete de outros idiomas ou outras especificidades.', 'iande')"/>
             <p v-html="__('A <b>etapa 3</b> é a confirmação do agendamento, validada pelo próprio Museu a ser visitado. Com a etapa 3 finalizada, seu agendamento estará garantido.', 'iande')"/>
