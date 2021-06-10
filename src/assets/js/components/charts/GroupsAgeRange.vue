537afff4b8b6a3966936e082a3f4ce2d64e5ca4c
everaldomatias <matiaseveraldo@gmail.com>
2021-06-10 16:47:01 -0300

    Chart Groups Age Range

--- src/assets/js/components/charts/GroupsAgeRange.vue
+++ src/assets/js/components/charts/GroupsAgeRange.vue
@@ -14,14 +14,65 @@
             groups: { type: Array, required: true },
         },
         computed: {
-            filterData () {
-                return []
+            chartData () {
+
+                let chartData = this.groups.reduce((increment, group) => {
+
+                    if (typeof increment[group.age_range] !== 'undefined') {
+                        increment[group.age_range].data = increment[group.age_range].data + 1
+                    } else {
+                        increment[group.age_range] = {
+                            label: group.age_range,
+                            data: 1
+                        }
+                    }
+
+                    return increment
+                }, [])
+
+                return chartData
+
             },
             options () {
-                return {}
+                return {
+                    plotOptions: {
+                        bar: {
+                            horizontal: false,
+                            columnWidth: '90%',
+                            dataLabels: {
+                                position: 'top', // top, center, bottom
+                            }
+                        }
+                    },
+                    dataLabels: {
+                        enabled: true,
+                        offsetY: -30,
+                        style: {
+                            fontSize: '12px',
+                            colors: ['var(--iande-text-color)']
+                        }
+                    },
+                    xaxis: {
+                        categories: Object.values(this.chartData).map(d => d.label),
+                    },
+                    colors: ['#1E2E55'],
+                    states: {
+                        hover: {
+                            filter: {
+                                type: 'darken',
+                                value: 0.9
+                            }
+                        }
+                    }
+                }
             },
             series () {
-                return []
+                return [
+                    {
+                        name: __('Grupos', 'iande'),
+                        data: Object.values(this.chartData).map(d => d.data)
+                    }
+                ]
             },
         },
     }
