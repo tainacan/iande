48659c964df0e6d90eb5cb0c54eb9a033d2aec97
Leonardo <leopiccionia@gmail.com>
2021-06-18 17:27:00 -0300

    Validate user input

--- src/includes/itinerary-metabox.php
+++ src/includes/itinerary-metabox.php
@@ -36,8 +36,9 @@ function build_itinerary_metabox($post) {
  * @link https://codex.wordpress.org/Plugin_API/Action_Reference/save_post
  */
 function save_itinerary_metabox($post_id) {
-    if (!empty($_POST['tainacan-meta'])) {
-        \update_post_meta($post_id, 'tainacan_meta', $_POST['tainacan-meta']);
+    $tainacan_meta = \filter_var($_POST['tainacan-meta'] ?: '');
+    if (!empty($tainacan_meta)) {
+        \update_post_meta($post_id, 'tainacan_meta', $tainacan_meta);
     }
 }
 \add_action('save_post_exhibition', 'IandePlugin\\save_itinerary_metabox', 10, 1);
