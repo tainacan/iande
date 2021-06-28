4553eaeb134a8f5e2068bb48926c5f80c4497108
Leonardo <leopiccionia@gmail.com>
2021-06-28 16:11:40 -0300

    Filter cities chart by state

--- src/assets/js/components/charts/Cities.vue
+++ src/assets/js/components/charts/Cities.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="iande-chart-wrapper">
         <h2>{{ __('Cidades de origem dos grupos', 'iande') }}</h2>
+        <select v-model="state">
+            <option v-for="(name, uf) of stateOptions" :key="uf" :value="uf">{{ name }}</option>
+        </select>
         <ApexChart type="bar" :series="series" :options="options"/>
     </div>
 </template>
@@ -10,6 +13,7 @@
     import { sortBy } from '@utils'
 
     import cities from '../../../json/municipios.json'
+    import states from '../../../json/estados.json'
 
     export default {
         name: 'CitiesChart',
@@ -17,26 +21,45 @@
             groups: { type: Array, required: true },
             institutions: { type: Object, required: true },
         },
+        data () {
+            return {
+                state: '',
+            }
+        },
         computed: {
             categories () {
                 return this.sortedCities.map(city => cities[city])
             },
-            cities () {
+            chartData () {
                 const chartData = {}
 
                 for (const group of this.groups) {
                     const city = this.getCity(group)
+                    const state = this.getState(group)
 
-                    if (city) {
-                        if (chartData[city]) {
-                            chartData[city] += 1
+                    if (city && state) {
+                        if (!chartData[state]) {
+                            chartData[state] = { count: 0, items: {} }
+                        }
+
+                        const stateData = chartData[state]
+                        stateData.count++
+                        if (stateData.items[city]) {
+                            stateData.items[city] += 1
                         } else {
-                            chartData[city] = 1
+                            stateData.items[city] = 1
                         }
                     }
                 }
 
                 return chartData
+            },
+            cities () {
+                if (this.state) {
+                    return this.chartData[this.state]?.items || {}
+                } else {
+                    return {}
+                }
             },
             options () {
                 return {
@@ -81,10 +104,35 @@
                 ]
             },
             sortedCities () {
-                return Object.entries(this.cities).sort(sortBy(x => x[1])).map(x => x[0])
+                return Object.entries(this.cities)
+                    .filter(x => x[0].startsWith(this.state))
+                    .sort(sortBy(x => x[1]))
+                    .map(x => x[0])
+            },
+            stateOptions () {
+                const entries = Object.values(states)
+                    .map(state => [state.sigla, state.nome])
+                    .sort(sortBy(state => state[1]))
+                return Object.fromEntries(entries)
             },
         },
+        created () {
+            this.computeMainState()
+        },
         methods: {
+            computeMainState () {
+                let maxCount = 0
+                let popularState = ''
+
+                for (const [state, stateData] of Object.entries(this.chartData)) {
+                    if (stateData.count > maxCount) {
+                        popularState = state
+                        maxCount = stateData.count
+                    }
+                }
+
+                this.state = popularState
+            },
             getCity (group) {
                 const institution = this.institutions[group.ID]
                 if (!institution) {
@@ -92,6 +140,14 @@
                 }
 
                 return institution.city
+            },
+            getState (group) {
+                const institution = this.institutions[group.ID]
+                if (!institution) {
+                    return null
+                }
+
+                return institution.state
             },
         },
     }
