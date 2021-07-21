34a78f692aca768cab280ff27af8e7d340ce4a4c
Leonardo <leopiccionia@gmail.com>
2021-07-21 18:35:13 -0300

    I18n: group details

--- src/assets/js/components/GroupDetails.vue
+++ src/assets/js/components/GroupDetails.vue
@@ -13,7 +13,7 @@
                     <div>
                         <div class="iande-appointment__info">
                             <Icon :icon="['far', 'image']"/>
-                            <span>{{ __(exhibition.title, 'title') }}</span>
+                            <span>{{ __(exhibition.title, 'iande') }}</span>
                         </div>
                         <div class="iande-appointment__info">
                             <Icon :icon="['far', 'clock']"/>
@@ -27,9 +27,9 @@
                                     <Icon icon="check"/>
                                 </div>
                                 <label>
-                                    <span>Mediação:</span>
+                                    <span>{{ __('Mediação:', 'iande') }}</span>
                                     <select v-model="group.educator_id">
-                                        <option :value="null">Atribuir mediação</option>
+                                        <option :value="null">{{ __('Atribuir mediação', 'iande') }}</option>
                                         <option v-for="educator of educators" :key="educator.ID" :value="educator.ID">
                                             {{ educator.display_name }}
                                         </option>
@@ -41,15 +41,15 @@
                                 <div class="iande-group__step-icon" :class="{ active: !!group.has_checkin }">
                                     <Icon :icon="group.has_checkin ? 'check' : 'minus'"/>
                                 </div>
-                                Check-in
+                                {{ __('Check-in', 'iande') }}
                             </div>
                             <div class="iande-group__step">
                                 <div class="iande-group__step-icon" :class="{ active: !!group.has_report }">
                                     <Icon :icon="group.has_report ? 'check' : 'minus'"/>
                                 </div>
-                                Avaliação
+                                {{ __('Avaliação', 'iande') }}
                             </div>
-                            <div class="iande-appointment__toggle" :aria-label="showDetails ? 'Ocultar detalhes' : 'Exibir detalhes'" role="button" tabindex="0" v-if="boxed" @click="toggleDetails" @keypress.enter="toggleDetails">
+                            <div class="iande-appointment__toggle" :aria-label="showDetails ? __('Ocultar detalhes', 'iande') : __('Exibir detalhes', 'iande')" role="button" tabindex="0" v-if="boxed" @click="toggleDetails" @keypress.enter="toggleDetails">
                                 <Icon :icon="showDetails ? 'minus-circle' : 'plus-circle'"/>
                             </div>
                         </div>
@@ -64,16 +64,16 @@
                         <h3><Icon icon="users"/> {{ name }}</h3>
                     </div>
                     <div>{{ group.age_range }}</div>
-                    <div>previsão de {{ group.num_people }} visitantes</div>
-                    <div>{{ group.num_responsible == 1 ? '1 responsável' : `${group.num_responsible} responsáveis` }}</div>
+                    <div>{{ sprintf(__('previsão de %s visitantes', 'iande'), group.num_people) }}</div>
+                    <div>{{ sprintf(_n('%s responsável', '%s responsáveis', group.num_responsible, 'iande'), group.num_responsible) }}</div>
                     <div>{{ group.scholarity }}</div>
-                    <div>Deficiências: {{ disabilities }}</div>
-                    <div>Idiomas: {{ languages }}</div>
+                    <div>{{ __('Deficiências', 'iande') }}: {{ disabilities }}</div>
+                    <div>{{ __('Idiomas', 'iande') }}: {{ languages }}</div>
                 </div>
 
                 <div class="iande-appointment__box">
                     <div class="iande-appointment__box-title">
-                        <h3><Icon icon="user"/>Responsável pela visita</h3>
+                        <h3><Icon icon="user"/>{{ __('Responsável pela visita', 'iande') }}</h3>
                     </div>
                     <div>
                         <div>{{ appointment.responsible_first_name }} {{ appointment.responsible_last_name }}</div>
@@ -87,24 +87,24 @@
 
                 <div class="iande-appointment__box">
                     <div class="iande-appointment__box-title">
-                        <h3><Icon :icon="['far', 'address-card']"/>Dados adicionais</h3>
+                        <h3><Icon :icon="['far', 'address-card']"/>{{ __('Dados adicionais', 'iande') }}</h3>
                     </div>
-                    <div>Você já visitou o museu antes: {{ formatBinaryOption(appointment.has_visited_previously) }}</div>
-                    <div>Preparação: {{ formatBinaryOption(appointment.has_prepared_visit) }}</div>
-                    <div v-if="appointment.additional_comment">Comentários: {{ appointment.additional_comment }}</div>
+                    <div>{{ __('Você já visitou o museu antes', 'iande') }}: {{ formatBinaryOption(appointment.has_visited_previously) }}</div>
+                    <div>{{ __('Preparação', 'iande') }}: {{ formatBinaryOption(appointment.has_prepared_visit) }}</div>
+                    <div v-if="appointment.additional_comment">{{ __('Comentários', 'iande') }}: {{ appointment.additional_comment }}</div>
                 </div>
             </div>
             <div class="iande-appointment__buttons" v-if="isEducator">
                 <a class="iande-button" :class="canEvaluate ? 'solid' : 'primary'" :href="$iandeUrl(`group/checkin?ID=${group.ID}`)" v-if="canCheckin">
-                    {{ group.has_checkin === 'on' ? 'Editar check-in' : 'Fazer-checkin' }}
+                    {{ group.has_checkin === 'on' ? __('Editar check-in', 'iande') : __('Fazer-checkin', 'iande') }}
                 </a>
                 <a class="iande-button primary" :href="$iandeUrl(`group/report?ID=${group.ID}`)" v-if="canEvaluate">
-                    Avaliar visita
+                    {{ __('Avaliar visita', 'iande') }}
                 </a>
             </div>
         </div>
         <div class="iande-group__toggle-button" role="button" tabindex="0" v-if="boxed" @click="toggleDetails" @keypress.enter="toggleDetails">
-            {{ collapsed ? 'Exibir detalhes' : 'Ocultar detalhes' }}
+            {{ collapsed ? __('Exibir detalhes', 'iande') : __('Ocultar detalhes', 'iande') }}
         </div>
     </section>
 </template>
@@ -113,10 +113,24 @@
     import { DateTime } from 'luxon'
     import { get } from 'vuex-pathify'
 
+    import { __, _x, sprintf } from '@plugins/wp-i18n'
     import { api, formatPhone, isOther, today } from '@utils'
     import { assignmentStatus } from '@utils/groups'
 
-    const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+    const months = [
+        _x('Jan', 'month', 'iande'),
+        _x('Fev', 'month', 'iande'),
+        _x('Mar', 'month', 'iande'),
+        _x('Abr', 'month', 'iande'),
+        _x('Mai', 'month', 'iande'),
+        _x('Jun', 'month', 'iande'),
+        _x('Jul', 'month', 'iande'),
+        _x('Ago', 'month', 'iande'),
+        _x('Set', 'month', 'iande'),
+        _x('Out', 'month', 'iande'),
+        _x('Nov', 'month', 'iande'),
+        _x('Dez', 'month', 'iande')
+    ]
 
     export default {
         name: 'GroupDetails',
@@ -151,14 +165,14 @@
             disabilities () {
                 const disabilities = this.group.disabilities
                 if (!disabilities || disabilities.length === 0) {
-                    return 'não'
+                    return __('Não', 'iande')
                 } else {
                     return disabilities
                         .map(disability => {
                             if (isOther(disability.disabilities_type) && disability.disabilities_other) {
-                                return `${disability.disabilities_type} / ${disability.disabilities_other} (${disability.disabilities_count})`
+                                return `${__(disability.disabilities_type, 'iande')} / ${__(disability.disabilities_other, 'iande')} (${disability.disabilities_count})`
                             } else {
-                                return `${disability.disabilities_type} (${disability.disabilities_count})`
+                                return `${__(disability.disabilities_type, 'iande')} (${disability.disabilities_count})`
                             }
                         })
                         .join(', ')
@@ -166,7 +180,7 @@
             },
             endHour () {
                 const delta = { minutes: Number(this.exhibition.duration) }
-                return DateTime.fromFormat(this.group.hour, 'HH:mm').plus(delta).toFormat('HH:mm')
+                return DateTime.fromFormat(this.group.hour, 'HH:mm').plus(delta).toFormat(__('HH:mm', 'iande'))
             },
             exhibition () {
                 return this.exhibitions.find(exhibition => exhibition.ID == this.group.exhibition_id)
@@ -177,12 +191,12 @@
             },
             languages () {
                 const languages = this.group.languages
-                return [{ languages_name: 'Português' }, ...(languages ?? [])]
+                return [{ languages_name: __('Português', 'iande') }, ...(languages ?? [])]
                     .map(language => {
                         if (isOther(language.languages_name) && language.languages_other) {
-                            return `${language.languages_name} / ${language.languages_other}`
+                            return `${__(language.languages_name, 'iande')} / ${__(language.languages_other, 'iande')}`
                         } else {
-                            return language.languages_name
+                            return __(language.languages_name, 'iande')
                         }
                     })
                     .join(', ')
@@ -192,8 +206,8 @@
                 return months[parseInt(parts[1]) - 1]
             },
             name () {
-                const appointmentName = this.appointment.name || `Agendamento ${this.appointment.ID}`
-                const groupName = this.group.name || `Grupo ${this.group.ID}`
+                const appointmentName = this.appointment.name || sprintf(__('Agendamento %s', 'iande'), this.appointment.ID)
+                const groupName = this.group.name || sprintf(__('Grupo %s', 'iande'), this.group.ID)
                 return `${appointmentName} / ${groupName}`
             },
             responsibleRole () {
@@ -222,7 +236,7 @@
         },
         methods: {
             formatBinaryOption (option) {
-                return option === 'yes' ? 'Sim' : 'Não'
+                return option === 'yes' ? __('Sim', 'iande') : __('Não', 'iande')
             },
             formatPhone,
             toggleDetails () {
