3b382ab6cbda319b0d46adcf575a1db12b17e755
Leonardo <leopiccionia@gmail.com>
2021-07-27 16:53:15 -0300

    Refactor confirmation steps to own components

---
+++ src/assets/js/components/ItineraryDetails.vue
@@ -0,0 +1,85 @@
+<template>
+    <form class="iande-form iande-stack stack-lg" @submit.prevent>
+        <h1>{{ __('Finalizando roteiro', 'iande') }}</h1>
+
+        <div>
+            <label for="name" class="iande-label">{{ __('Título do roteiro', 'iande') }}</label>
+            <Input id="name" type="text" :placeholder="__('Título do roteiro', 'iande')" v-model="itinerary.name" :validations="v.itinerary.name"/>
+        </div>
+
+        <div>
+            <label for="cover" class="iande-label">{{ __('Imagem de capa', 'iande') }}</label>
+            <FileUploader id="cover" accept="image/*" v-model="itinerary.cover" :validations="v.itinerary.cover"/>
+        </div>
+
+        <div>
+            <label for="description" class="iande-label">{{ __('Descrição breve', 'iande') }}<span class="iande-label__optional">{{ __('(opcional)', 'iande') }}</span></label>
+            <TextArea id="description" v-model="itinerary.description" :validations="v.itinerary.description"/>
+        </div>
+
+        <div>
+            <label for="publicly_findable" class="iande-label">{{ __('O roteiro será aberto ao público ou restrito a quem tiver o link de acesso?', 'iande') }}</label>
+            <RadioGroup id="publicly_findable" v-model="itinerary.publicly_findable" :options="publicOptions" :validations="v.itinerary.publicly_findable"/>
+        </div>
+
+        <div>
+            <label for="shareable" class="iande-label">{{ __('Você deseja deixar seu roteiro compartilhável?', 'iande') }}</label>
+            <RadioGroup id="shareable" v-model="itinerary.shareable" :options="shareOptions" :validations="v.itinerary.shareable"/>
+        </div>
+
+        <div>
+            <label for="layout" class="iande-label">{{ __('Escolha o layout de visualização desktop do roteiro', 'iande') }}</label>
+            <LayoutSelector id="layout" v-model="itinerary.layout" :validations="v.itinerary.layout"/>
+        </div>
+
+        <div class="iande-form-error" v-if="formError">
+            <span>{{ __(formError, 'iande') }}</span>
+        </div>
+
+        <div class="iande-form-grid">
+            <a class="iande-button solid" :href="$iandeUrl('itinerary/list')">
+                {{ __('Cancelar', 'iande') }}
+            </a>
+
+            <button type="button" class="iande-button primary" @click="$emit('update')">
+                {{ __('Salvar alterações', 'iande') }}
+            </button>
+        </div>
+    </form>
+</template>
+
+<script>
+    import FileUploader from '@components/FileUploader.vue'
+    import Input from '@components/Input.vue'
+    import LayoutSelector from '@components/LayoutSelector.vue'
+    import RadioGroup from '@components/RadioGroup.vue'
+    import TextArea from '@components/TextArea.vue'
+    import { __ } from '@plugins/wp-i18n'
+    import { constant } from '@utils'
+
+    export default {
+        name: 'ItineraryDetails',
+        components: {
+            FileUploader,
+            Input,
+            LayoutSelector,
+            RadioGroup,
+            TextArea,
+        },
+        props: {
+            formError: { type: String, default: '' },
+            itinerary: { type: Object, required: true },
+            v: { type: Object, required: true },
+        },
+        computed: {
+            publicOptions: constant({
+                [__('Aberto ao público', 'iande')]: 'yes',
+                [__('Acesso apenas com link', 'iande')]: 'no',
+            }),
+            shareOptions: constant({
+                [__('Não', 'iande')]: 'no',
+                [__('Sim', 'iande')]: 'yes',
+            }),
+        },
+    }
+</script>
--- src/assets/js/pages/confirm-itinerary.vue
+++ src/assets/js/pages/confirm-itinerary.vue
@@ -37,50 +37,12 @@
                 </div>
             </aside>
             <main>
-                <div class="iande-container narrow mt-lg">
-                    <form class="iande-form iande-stack stack-lg" @submit.prevent v-if="itinerary">
+                <div class="iande-container narrow mt-lg" v-if="itinerary">
+                    <ItineraryDetails :formError="formError" :itinerary="itinerary" :v="$v" @update="update" v-if="view === 'settings'"/>
+                    <div class="iande-stack stack-lg" v-if="view === 'items'">
                         <h1>{{ __('Finalizando roteiro', 'iande') }}</h1>
 
-                        <div>
-                            <label for="name" class="iande-label">{{ __('Título do roteiro', 'iande') }}</label>
-                            <Input id="name" type="text" :placeholder="__('Título do roteiro', 'iande')" v-model="itinerary.name" :validations="$v.itinerary.name"/>
-                        </div>
-
-                        <div>
-                            <label for="cover" class="iande-label">{{ __('Imagem de capa', 'iande') }}</label>
-                            <FileUploader id="cover" accept="image/*" v-model="itinerary.cover" :validations="$v.itinerary.cover"/>
-                        </div>
-
-                        <div>
-                            <label for="description" class="iande-label">{{ __('Descrição breve', 'iande') }}<span class="iande-label__optional">{{ __('(opcional)', 'iande') }}</span></label>
-                            <TextArea id="description" v-model="itinerary.description" :validations="$v.itinerary.description"/>
-                        </div>
-
-                        <div>
-                            <label for="publicly_findable" class="iande-label">{{ __('O roteiro será aberto ao público ou restrito a quem tiver o link de acesso?', 'iande') }}</label>
-                            <RadioGroup id="publicly_findable" v-model="itinerary.publicly_findable" :options="publicOptions" :validations="$v.itinerary.publicly_findable"/>
-                        </div>
-
-                        <div>
-                            <label for="shareable" class="iande-label">{{ __('Você deseja deixar seu roteiro compartilhável?', 'iande') }}</label>
-                            <RadioGroup id="shareable" v-model="itinerary.shareable" :options="shareOptions" :validations="$v.itinerary.shareable"/>
-                        </div>
-
-                        <div>
-                            <label for="layout" class="iande-label">{{ __('Escolha o layout de visualização desktop do roteiro', 'iande') }}</label>
-                            <LayoutSelector id="layout" v-model="itinerary.layout" :validations="$v.itinerary.layout"/>
-                        </div>
-
-                        <div class="iande-form-grid">
-                            <a class="iande-button solid" :href="$iandeUrl('itinerary/list')">
-                                {{ __('Cancelar', 'iande') }}
-                            </a>
-
-                            <button type="button" class="iande-button primary">
-                                {{ __('Salvar alterações', 'iande') }}
-                            </button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </main>
         </article>
@@ -90,22 +52,13 @@
 <script>
     import { required } from 'vuelidate/lib/validators'
 
-    import FileUploader from '@components/FileUploader.vue'
-    import Input from '@components/Input.vue'
-    import LayoutSelector from '@components/LayoutSelector.vue'
-    import RadioGroup from '@components/RadioGroup.vue'
-    import TextArea from '@components/TextArea.vue'
-    import { __ } from '@plugins/wp-i18n'
-    import { api, constant, qs } from '@utils'
+    import ItineraryDetails from '@components/ItineraryDetails.vue'
+    import { api, qs } from '@utils'
 
     export default {
         name: 'ConfirmItineraryPage',
         components: {
-            FileUploader,
-            Input,
-            LayoutSelector,
-            RadioGroup,
-            TextArea,
+            ItineraryDetails,
         },
         data () {
             return {
@@ -117,16 +70,6 @@
                 menuCollapsed: true,
                 view: 'settings',
             }
-        },
-        computed: {
-            publicOptions: constant({
-                [__('Aberto ao público', 'iande')]: 'yes',
-                [__('Acesso apenas com link', 'iande')]: 'no',
-            }),
-            shareOptions: constant({
-                [__('Não', 'iande')]: 'no',
-                [__('Sim', 'iande')]: 'yes',
-            }),
         },
         validations: {
             itinerary: {
@@ -158,6 +101,7 @@
         methods: {
             async remove () {
                 try {
+                    this.formError = ''
                     await api.post('itinerary/delete', { ID: this.itinerary.ID })
                     window.location.assign(this.$iandeUrl('itinerary/list'))
                 } catch (err) {
@@ -166,6 +110,7 @@
             },
             async update () {
                 try {
+                    this.formError = ''
                     await api.post('itinerary/update', this.itinerary)
                     window.location.assign(this.$iandeUrl('itinerary/list'))
                 } catch (err) {
