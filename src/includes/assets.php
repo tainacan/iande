0d1c701203a1a1001785dbcba5f9943e7a7d7a29
Leonardo <leopiccionia@gmail.com>
2021-12-15 10:33:40 -0300

    Update Tainacan enqueue
    In Tainacan 0.18.6 or 0.18.7, the script name changed

--- src/includes/assets.php
+++ src/includes/assets.php
@@ -11,6 +11,6 @@ function enqueue_assets() {
     \wp_enqueue_style('iande', IANDE_PLUGIN_DISTURL . 'app.css', [], IANDE_PLUGIN_VERSION);
     \wp_enqueue_script('iande', IANDE_PLUGIN_DISTURL . 'app.js', ['wp-i18n'], IANDE_PLUGIN_VERSION, true);
     if (\get_query_var('iande_controller') === 'itinerary') {
-        \wp_enqueue_script('iande-tainacan', IANDE_PLUGIN_DISTURL . 'tainacan-view-modes.js', ['underscore', 'tainacan-blocks-common-theme-scripts', 'wp-i18n'], IANDE_PLUGIN_VERSION, true);
+        \wp_enqueue_script('iande-tainacan', IANDE_PLUGIN_DISTURL . 'tainacan-view-modes.js', ['underscore', 'tainacan-blocks-common-scripts', 'wp-i18n'], IANDE_PLUGIN_VERSION, true);
     }
 }
