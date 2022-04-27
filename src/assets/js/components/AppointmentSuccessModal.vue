20fa929dfe3a1c4d5e15e970d5cbe5c98c7dd3ff
Leonardo <leopiccionia@gmail.com>
2022-04-27 15:20:36 -0300

    Improve text of confirmation message

--- src/assets/js/components/AppointmentSuccessModal.vue
+++ src/assets/js/components/AppointmentSuccessModal.vue
@@ -1,8 +1,8 @@
 <template>
     <Modal ref="modal" :label="__('Sucesso!', 'iande')" narrow @close="close">
         <div class="iande-stack">
-            <h1>{{ __('Agendamento enviado com sucesso!', 'iande') }}</h1>
-            <p>{{ __('Os dados do seu agendamento foram enviados para o museu. Assim que a sua visita for confirmada, você receberá um email com todos os detalhes.', 'iande') }}</p>
+            <h1>{{ __('Solicitação enviada com sucesso!', 'iande') }}</h1>
+            <p>{{ __('A solicitação para seu agendamento foi enviada para o museu. Assim que a sua visita for confirmada, você receberá um email com todos os detalhes.', 'iande') }}</p>
             <button class="iande-button solid" @click="close">
                 {{ __('Voltar aos agendamentos', 'iande') }}
             </button>
