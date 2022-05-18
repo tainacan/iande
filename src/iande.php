02f36ca9a6b56d274e0f99561b8544c397c1048b
Leonardo <leopiccionia@gmail.com>
2022-05-18 10:42:35 -0300

    Release v0.15.5

--- src/iande.php
+++ src/iande.php
@@ -7,7 +7,7 @@
  * Plugin Name:       Iandé
  * Plugin URI:        https://iandecultura.com.br/
  * Description:       Agendamento de visitas de grupos para instituições que recebem públicos presencial ou digitalmente.
- * Version:           0.15.4
+ * Version:           0.15.5
  * Author:            Percebe
  * Author URI:        https://percebeeduca.com.br/
  * License:           GPL-2.0+
@@ -26,7 +26,7 @@ if (!defined('WPINC')) {
 /**
  * Currently plugin version.
  */
-define('IANDE_PLUGIN_VERSION', '0.15.4');
+define('IANDE_PLUGIN_VERSION', '0.15.5');
 
 define('IANDE_PLUGIN_BASEPATH', plugin_dir_path(__FILE__));
 define('IANDE_PLUGIN_BASEURL', plugins_url('', __FILE__) . '/');
